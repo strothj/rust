0d4dcd7fa9d0697bc9dfbadccb65a6fe9c2c2a95
strothj <jason@jasons.io>
2018-03-23 11:24:26 -0400

    ch 7

---
+++ 07-modules/communicator/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod client;
+
+pub mod network;
+
+#[cfg(test)]
+mod tests {
+    use super::client;
+
+    #[test]
+    fn it_works() {
+        // assert_eq!(2 + 2, 4);
+        // super::client::connect();
+        client::connect();
+    }
+}
